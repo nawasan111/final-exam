626fb06a3d97cd07339b20f75a21838fcdd0e603
Nawasan Wisitsingkhon <nawasan.dev@gmail.com>
2023-10-07 15:08:37 +0700

    change word in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,14 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <Box sx={{ p: 3, display: "flex", justifyContent: "end" , alignItems: "center"}}>
+    <Box
+      sx={{
+        p: 3,
+        display: "flex",
+        justifyContent: "end",
+        alignItems: "center",
+      }}
+    >
       <a
         className="flex items-center text-slate-800 font-medium no-underline hover:underline md:mx-5"
         href="https://github.com/nawasan111/final-exam"
@@ -15,7 +22,7 @@ export default function Footer() {
         className="text-slate-800 font-medium no-underline hover:underline md:mx-5"
         href="https://github.com/Arikato111"
       >
-        Auther
+        Developer
       </a>
     </Box>
   );
