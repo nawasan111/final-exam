db2a40402ba552faafc3ccd5bf259929f0943989
Nawasan Wisitsingkhon <nawasan.dev@gmail.com>
2023-10-07 14:59:09 +0700

    fix: position align in footer;

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <Box sx={{ p: 3, display: "flex", justifyContent: "end" }}>
+    <Box sx={{ p: 3, display: "flex", justifyContent: "end" , alignItems: "center"}}>
       <a
         className="flex items-center text-slate-800 font-medium no-underline hover:underline md:mx-5"
         href="https://github.com/nawasan111/final-exam"
