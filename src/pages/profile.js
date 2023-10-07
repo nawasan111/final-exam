66d1236f84d9c1bb2a080dab8bb8ba84921a21e9
Nawasan Wisitsingkhon <nawasan.dev@gmail.com>
2023-10-07 20:12:34 +0700

    show name in title in profile page

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -8,6 +8,7 @@ import {
 } from "@mui/material";
 import React, { useContext } from "react";
 import { UserContext } from "./_app";
+import Head from "next/head";
 
 export default function Profile() {
   const user = useContext(UserContext);
@@ -17,49 +18,57 @@ export default function Profile() {
   return (
     <>
       {user.value?.token ? (
-        <Box>
-          <Paper sx={{ p: 3, boxShadow: "none" }}>
-            <div className="gap-10  mx-auto text-left grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 max-w-[1520px]">
-              <div className="mx-auto">
-                <img
-                  className="inline-block w-[200px] h-[200px] sm:w-[300px] sm:h-[300px] rounded-lg object-cover"
-                  src={user.value.photo.length ? user.value.photo : "empty.jpg"}
-                  alt=""
-                />
+        <>
+          <Head>
+            <title>{`${user.value.name} | OpenShop`}</title>
+          </Head>
+
+          <Box>
+            <Paper sx={{ p: 3, boxShadow: "none" }}>
+              <div className="gap-10  mx-auto text-left grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 max-w-[1520px]">
+                <div className="mx-auto">
+                  <img
+                    className="inline-block w-[200px] h-[200px] sm:w-[300px] sm:h-[300px] rounded-lg object-cover"
+                    src={
+                      user.value.photo.length ? user.value.photo : "empty.jpg"
+                    }
+                    alt=""
+                  />
+                </div>
+                <div className="lg:col-span-2 p-3 text-lg overflow-x-scroll">
+                  <Table>
+                    <TableBody>
+                      <TableRow>
+                        <TableCell sx={{ maxWidth }}>ชื่อ</TableCell>
+                        <TableCell>{user.value.name}</TableCell>
+                      </TableRow>
+                      <TableRow>
+                        <TableCell sx={{ maxWidth }}>อีเมล</TableCell>
+                        <TableCell>{user.value.email}</TableCell>
+                      </TableRow>
+                      <TableRow>
+                        <TableCell sx={{ maxWidth }}>username</TableCell>
+                        <TableCell>@{user.value.username}</TableCell>
+                      </TableRow>
+                      <TableRow>
+                        <TableCell sx={{ maxWidth }}>เบอร์โทร</TableCell>
+                        <TableCell>{user.value.phone}</TableCell>
+                      </TableRow>
+                      <TableRow>
+                        <TableCell sx={{ maxWidth }}>ที่อยู่</TableCell>
+                        <TableCell>
+                          {!!user.value.address
+                            ? user.value.address
+                            : "ยังไม่ระบุ"}
+                        </TableCell>
+                      </TableRow>
+                    </TableBody>
+                  </Table>
+                </div>
               </div>
-              <div className="lg:col-span-2 p-3 text-lg overflow-x-scroll">
-                <Table>
-                  <TableBody>
-                    <TableRow>
-                      <TableCell sx={{ maxWidth }}>ชื่อ</TableCell>
-                      <TableCell>{user.value.name}</TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell sx={{ maxWidth }}>อีเมล</TableCell>
-                      <TableCell>{user.value.email}</TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell sx={{ maxWidth }}>username</TableCell>
-                      <TableCell>@{user.value.username}</TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell sx={{ maxWidth }}>เบอร์โทร</TableCell>
-                      <TableCell>{user.value.phone}</TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell sx={{ maxWidth }}>ที่อยู่</TableCell>
-                      <TableCell>
-                        {!!user.value.address
-                          ? user.value.address
-                          : "ยังไม่ระบุ"}
-                      </TableCell>
-                    </TableRow>
-                  </TableBody>
-                </Table>
-              </div>
-            </div>
-          </Paper>
-        </Box>
+            </Paper>
+          </Box>
+        </>
       ) : (
         <Box>
           <Paper sx={{ p: 3, textAlign: "center" }}>
