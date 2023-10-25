4e9f67b8de9501eb7da9bc842beb7fedaff43495
Nawasan Wisitsingkhon <nawasan.dev@gmail.com>
2023-10-25 19:52:45 +0700

    jwt has expires

--- src/components/lib/jwttoken.js
+++ src/components/lib/jwttoken.js
@@ -4,7 +4,10 @@ import jwt from "jsonwebtoken";
  * @param {{id: number, name: string, email: string, phone: string, photo: string, username: string  }} data
  */
 export const JwtGenerate = (data) => {
-  let token = jwt.sign(data, process.env.JWT_TOKEN, { algorithm: "HS256" });
+  let token = jwt.sign(data, process.env.JWT_TOKEN, {
+    algorithm: "HS256",
+    expiresIn: "2d",
+  });
   return token;
 };
 
